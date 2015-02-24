d835652cc3176fb6ae6e53ebb2701c7b1e586046
Samantha Koehn <samantha-koehn@uiowa.edu>
2015-02-24 10:53:18 -0600

    basic calculation no tax

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,4 +2,10 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+
+
+    Protected Sub btn_calculate_Click(sender As Object, e As EventArgs) Handles btn_calculate.Click
+        If (IsNumeric(tb_hoursWorked.Text) And IsNumeric(tb_hourlyWage.Text)) Then lbl_netPay.Text = tb_hoursWorked.Text * tb_hourlyWage.Text
+
+    End Sub
 End Class
